39dcf1ec4d8a95981e4698c29bf37a3ce776d3a1
zjakedev <z.jacobsdev@gmail.com>
2023-01-27 23:23:48 -0500

    added test folder

---
+++ tests/test_dashboard.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from ..server import app
+'''
+Testing API
+References: https://fastapi.tiangolo.com/tutorial/testing/
+'''
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
